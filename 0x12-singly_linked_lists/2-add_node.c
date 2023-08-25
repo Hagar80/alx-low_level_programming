06cab926e964fae381992330d9a6ae1101f87b04
Hagar <hagarwork07@gmail.com>
2023-08-24 18:16:26 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,10 +12,10 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	unsigned int len = 0
+	unsigned int len = 0;
 
-		while (str[len])
-			len++;
+	while (str[len])
+		len++;
 
 	new = malloc(sizeof(list_t));
 	if (!new)
